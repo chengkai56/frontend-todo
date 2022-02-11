4440fab243b88d42beeae9613a31fa6d6023e00e
unknown <chengkai.2nd@gmail.com>
2022-02-10 19:43:34 -0600

    version2

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
 // export const API_URL = 'http://localhost:8080'
-export const API_URL = 'https://backend-my-todo.herokuapp.com/'
+export const API_URL = 'https://backend-my-todo.herokuapp.com'
 export const JPA_API_URL = 'https://backend-my-todo.herokuapp.com/jpa'
 // export const JPA_API_URL = 'http://localhost:8080/jpa'
--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -1,15 +1,16 @@
 // import React, { Component } from 'react'
 import axios from 'axios'
+import { API_URL } from "../../Constants";
 
 class HelloWorldService {
 
     executeHelloWorldService() {
-        return axios.get('http://localhost:8080/hello-world')
+        return axios.get(`${API_URL}/hello-world`)//'http://localhost:8080/hello-world')
         // console.log('executed service')
     }
 
     executeHelloWorldBeanService() {
-        return axios.get('http://localhost:8080/hello-world-bean')
+        return axios.get(`${API_URL}/hello-world-bean`) //'http://localhost:8080/hello-world-bean')
     }
 
     executeHelloWorldPathVariableService(name) {
@@ -18,7 +19,7 @@ class HelloWorldService {
 
         // let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
 
-        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`
+        return axios.get(`${API_URL}/hello-world/path-variable/${name}`
             // // ,
             // //     {
             // //         headers: {
